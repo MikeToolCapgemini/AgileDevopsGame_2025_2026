eb72c02633e1ee808610a795db2023f5d4742a1e
Mike Tool <mike.tool@capgemini.com>
2025-10-28 10:52:41 +0100

    You can now give a save a name

--- scripts/Global/SaveSystem.gd
+++ scripts/Global/SaveSystem.gd
@@ -1,11 +1,28 @@
 extends Node
 
-var SavePath = "user://savegame.json"
+var SavePath = "user://saves/"
 var DefaultData = {}
 var SaveData = {}
+var currentSave
+@export var SaveNameField : LineEdit
+
+func _ready() -> void:
+	var saves = get_save_files()
+	if saves.size() > 0:
+		print("exsisting saves ", saves)
+	else:
+		print("No Saves Found")
 
 func save_game():
-	var file = FileAccess.open(SavePath, FileAccess.WRITE)
+	check_save_dir()
+	var saveName = get_save_name()
+	var savePath
+	if currentSave != saveName:
+		savePath = SavePath + currentSave + ".json"
+	else:
+		savePath = SavePath + saveName + ".json"
+		currentSave = saveName
+	var file = FileAccess.open(savePath, FileAccess.WRITE)
 	SaveData["DataDiscardedCards"] = GlobalSettings.DataDiscardedCards
 	SaveData["DiscardedCards"] = GlobalSettings.DiscardedCards
 	SaveData["BookmarkedCards"] = GlobalSettings.BookmarkedCards
@@ -14,9 +31,13 @@ func save_game():
 	file.store_string(json)
 	file.close()
 	print("Game saved...")
+	
+func loadSaveGames() :
+	pass
 
-func load_game():
-	var file = FileAccess.open(SavePath, FileAccess.READ)
+func load_game(saveName : String):
+	var save = SavePath + saveName + ".json"
+	var file = FileAccess.open(save, FileAccess.READ)
 	if (file == null):
 		reset_data()
 	var json = file.get_as_text()
@@ -27,11 +48,45 @@ func load_game():
 	GlobalSettings.BookmarkedCards = SaveData["BookmarkedCards"]
 	print("Game loaded...")
 
+func check_save_dir():
+	var persist_dir := DirAccess.open("user://")
+	if persist_dir:
+		if persist_dir.dir_exists("saves"):
+			pass
+		else:
+			persist_dir.make_dir("saves")
+	else:
+		printerr("An error occurred trying to open persistent user:// directory. Error: ", DirAccess.get_open_error())
+
+func get_save_files() -> Array:
+	check_save_dir()
+	var dir_path = "user://saves"
+	var save_files = []
+	var dir = DirAccess.open(dir_path)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if  file_name.ends_with(".json"):
+				save_files.append(file_name)
+			file_name = dir.get_next()
+		dir.list_dir_end()
+	return save_files
+
+func get_save_name() -> String:
+	var saveName
+	if SaveNameField.text != "":
+		saveName = SaveNameField.text
+	else:
+		var number = randi()% 100
+		saveName = "savegame"+ str(number)
+	return saveName
+
 func reset_data():
 	SaveData = DefaultData.duplicate(true)
 
 func _on_load_game_button_pressed():
-	load_game()
+	loadSaveGames()
 
 
 func _on_save_game_button_pressed() -> void:
