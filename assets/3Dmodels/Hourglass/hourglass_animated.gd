6b0284f7d78e9fa0cae8056abada871f2de62f9f
Mike Tool <miketooltje@gmail.com>
2025-12-24 16:11:13 +0100

    Fix to avoid double pressing start button
    it made it so the spin animation would get the reduced speed, from the flow animation

--- assets/3Dmodels/Hourglass/hourglass_animated.gd
+++ assets/3Dmodels/Hourglass/hourglass_animated.gd
@@ -7,6 +7,7 @@ var paused : bool = false
 var currentTime = 600
 var newTime = 600
 var timerRunning = false
+var spin_in_progress := false
 @export var EditMinObject : TextEdit
 @export var EditSecObject : TextEdit
 @export var InterruptUI : Node
@@ -27,7 +28,6 @@ func _process(delta):
 			setTimerRunning.rpc(false)
 		currentTime = currentTime - (1*delta)
 	
-	
 	var minutesText = int(currentTime) / 60
 	var secondsText = int(currentTime) % 60
 	var coolString = "[" + str(minutesText) + ":" + str(secondsText) + "]"
@@ -37,12 +37,21 @@ func _process(delta):
 var animation_duration : float
 @rpc("any_peer", "call_local")
 func _start_animation(target_duration : float):
+	if spin_in_progress:
+		return # Ignore double press completely
+	if animator.current_animation == "RESET":
+		return
 	if paused:
 		setTimerRunning.rpc(true)
 		animator.play()
 		paused = false
+		spin_in_progress = false
+		return
 	else:
+		_reset_animation()
+		reset_speed()
 		animator.queue("Spin")
+		spin_in_progress = true
 		animation_duration = target_duration
 		SpinTimer.start()
 		
@@ -51,15 +60,20 @@ func _start_animation(target_duration : float):
 @rpc("any_peer", "call_local")
 func _stop_animation():
 	paused = true
+	SpinTimer.stop()
 	animator.pause()
 	
 @rpc("any_peer", "call_local")
 func _reset_animation():
 	paused = false
+	SpinTimer.stop()
+	animator.stop()
+	animator.clear_queue()
 	animator.play("RESET")
 
 func _on_start_button_pressed():
-	_start_animation.rpc(newTime)
+	if !timerRunning:
+		_start_animation.rpc(newTime)
 		
 
 @rpc("any_peer", "call_local")
@@ -68,9 +82,12 @@ func reset_speed():
 
 func _on_reset_button_pressed():
 	updateTime.rpc(newTime)
+	SpinTimer.stop()
 	setTimerRunning.rpc(false)
 	reset_speed.rpc()
 	_reset_animation.rpc()
+	spin_in_progress = false
+	paused = false
 	pass # Replace with function body.
 
 func _on_stop_button_pressed():
@@ -103,6 +120,7 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 
 
 func _on_spin_timer_timeout() -> void:
+	spin_in_progress = false
 	setTimerRunning.rpc(true)
 	var anim_length = animator.get_animation("SandFlow").length
 	if animation_duration == null:
