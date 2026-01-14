7fc2670c0c346a02da52bee1fe4b052b77092c92
Mike Tool <miketooltje@gmail.com>
2026-01-14 14:56:48 +0100

    Sqlite database with standard user creation and login
    passwords still need to be hashed

---
+++ scripts/LoginDatabaseManager.gd
@@ -0,0 +1,39 @@
+extends RefCounted
+class_name  LDM
+
+var db
+
+
+func _init() -> void:
+	db = SQLite.new()
+	db.path = "user://data.db"
+	db.open_db()
+	var table = {
+		"id" : {"data_type": "int","primary_key": true, "not_null" : true, "auto_increment" : true},
+		"name": {"data_type" : "text"},
+		"password": {"data_type" : "text"},
+		"salt":{"data_type": "int", "not_null" : true}
+	}
+	
+	db.create_table("users", table)
+	pass
+
+
+func InsertUserData(name,password,salt):
+	var data = {
+		"name" : name,
+		"password" : password,
+		"salt" : salt
+	}
+	db.insert_row("users", data)
+
+
+func GetUserFromDB(username):
+	var query = db.query("SELECT salt,password,id from users where name = '" + username + "'")
+	for i in db.query_result:
+		return {
+			"id" : i["id"],
+			"hashedPassword" : i["password"],
+			"salt" : i["salt"],
+			"name" : username
+		}
--- scripts/UX/login.gd
+++ scripts/UX/login.gd
@@ -4,8 +4,39 @@ extends Control
 @export var UserNameField : LineEdit
 @export var PasswordField : LineEdit
 
+var Username : String
+var Password : String
 
+var ldm = LDM.new()
+
+
+func _get_TextBox_Values():
+	Username = UserNameField.text
+	Password = PasswordField.text
 
 
 func _on_Login_pressed() -> void:
-	pass # Replace with function body.
+	_get_TextBox_Values()
+	_check_user_information(Username,Password)
+	
+
+func createUser(username,password):
+	ldm.InsertUserData(username,password,131231)
+
+
+
+func _check_user_information(username,password):
+	var userData = ldm.GetUserFromDB(username)
+	
+	if userData == null:
+		ErrorLabel.show_error("Login Failed, invalid username")
+	else:
+		if userData["hashedPassword"] == password:
+			print(userData)
+		else:
+			ErrorLabel.show_error("Login Failed, invalid password")
+
+
+func _on_create_user_pressed() -> void:
+	_get_TextBox_Values()
+	createUser(Username,Password)
