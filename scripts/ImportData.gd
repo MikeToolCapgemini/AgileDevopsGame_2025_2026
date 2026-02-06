92ef9ceffe1b930bdf724286cfeea1688af649b7
Mike Tool <miketooltje@gmail.com>
2026-02-06 14:03:06 +0100

    importing data is now able to be updated in a folder on web

--- scripts/ImportData.gd
+++ scripts/ImportData.gd
@@ -1,9 +1,12 @@
 extends Node
 
+signal data_ready
+
 var PATH_JSON_DATA = "res://Data/csvjson.json"
-var CardData : Array
-#var path = "res://database/Cards.csv"
-#var TestPath = "C:/Users/miche/Desktop/PlaytestProject/DevOps_Game_v0.10/DevOps_Game_v0.9/database/Cards.csv"
+var WEB_PATH = "Data/csvjson.json"
+
+var CardData : Array = []
+var data_loaded := false
 
 var PlanCardData : Dictionary = {}
 var CodeCardData : Dictionary = {}
@@ -13,23 +16,138 @@ var ReleaseCardData : Dictionary = {}
 var DeployCardData : Dictionary = {}
 var OperateCardData : Dictionary = {}
 var MonitorCardData : Dictionary = {}
-var AllCardData = [PlanCardData, CodeCardData, BuildCardData, TestCardData, ReleaseCardData, DeployCardData, OperateCardData, MonitorCardData]
 
-func load_json_data():
-	#Find the json file
-	var file = PATH_JSON_DATA
-	#Read the json file and parse it as a string
-	var json_as_text = FileAccess.get_file_as_string(file)
-	var json = JSON.parse_string(json_as_text)
-	#Set the json string as an array
-	CardData = Array(json)
+var AllCardData = [
+	PlanCardData, CodeCardData, BuildCardData, TestCardData,
+	ReleaseCardData, DeployCardData, OperateCardData, MonitorCardData
+]
+
+
+func ensure_ready():
+	if data_loaded:
+		return
+	await data_ready
+
 
 func _ready():
+	load_json_data()
+
+
+func load_json_data():
+	if OS.has_feature("web"):
+		load_json_web()
+	else:
+		load_json_local()
+		finish_loading()
+
+
+func finish_loading():
 	import_data()
+	data_loaded = true
+	emit_signal("data_ready")
+
+
+func load_json_local():
+	var json_as_text = FileAccess.get_file_as_string(PATH_JSON_DATA)
+
+	if json_as_text == "" or json_as_text == null:
+		CardData = []
+		return
+
+	var json = JSON.parse_string(json_as_text)
+
+	if json == null:
+		CardData = []
+		return
+
+	CardData = Array(json)
+
+
+func get_web_base() -> String:
+	var protocol := "http"
+	var host := "localhost"
+
+	if OS.has_feature("web"):
+		var js := JavaScriptBridge
+		protocol = "https" if js.eval("window.location.protocol") == "https:" else "http"
+		host = js.eval("window.location.hostname")
+
+	return "%s://%s/" % [protocol, host]
+
+
+func load_json_web():
+	var base = get_web_base()
+	var url = base + WEB_PATH + "?t=" + str(Time.get_unix_time_from_system())
+
+	var http = HTTPRequest.new()
+	add_child(http)
+
+	http.request_completed.connect(_on_web_json_loaded)
+
+	if http.request(url) != OK:
+		load_json_local()
+		finish_loading()
+
+
+func _on_web_json_loaded(result, response_code, headers, body):
+
+	var text = body.get_string_from_utf8()
+
+	if result != HTTPRequest.RESULT_SUCCESS or response_code != 200:
+		load_json_local()
+		finish_loading()
+		return
+
+	var json = JSON.parse_string(text)
+
+	if json == null:
+		load_json_local()
+		finish_loading()
+		return
+
+	CardData = Array(json)
+	finish_loading()
+
+
+func import_data():
+
+	for d in AllCardData:
+		d.clear()
+
+	for Card in CardData:
+		if Card["Type"] != "Question" and Card["Type"] != "Discussion":
+			continue
+
+		match Card["Subtype"]:
+			"Plan":
+				PlanCardData[int(Card["UID"])] = Card
+
+			"Build":
+				BuildCardData[int(Card["UID"])] = Card
+
+			"Test":
+				TestCardData[int(Card["UID"])] = Card
+
+			"Deploy":
+				DeployCardData[int(Card["UID"])] = Card
+
+			"Code":
+				CodeCardData[int(Card["UID"])] = Card
+
+			"Release":
+				ReleaseCardData[int(Card["UID"])] = Card
+
+			"Operate":
+				OperateCardData[int(Card["UID"])] = Card
+
+			"Monitor":
+				MonitorCardData[int(Card["UID"])] = Card
+
 
 @rpc("any_peer","call_local")
 func sort_data():
 	import_data()
+
 	if !GlobalSettings.QuestionsBasic:
 		for cardDict in AllCardData:
 			sort_data_on_type("b", cardDict)
@@ -37,15 +155,13 @@ func sort_data():
 	if !GlobalSettings.QuestionsPro:
 		for cardDict in AllCardData:
 			sort_data_on_type("p", cardDict)
-	
+
 	if GlobalSettings.DataDiscardedCards.size() > 0:
 		for entry in GlobalSettings.DataDiscardedCards:
 			var type_string = entry.get("cardTypeString", null)
 			var dataDict = get_dict_on_subtype(type_string)
-			card_pop(dataDict,entry.get("keyVar"))
-			#discard the card again.
-			
-	
+			card_pop(dataDict, entry.get("keyVar"))
+
 
 func get_dict_on_subtype(subtype):
 	match subtype:
@@ -57,84 +173,13 @@ func get_dict_on_subtype(subtype):
 		"Deploy": return DeployCardData
 		"Operate": return OperateCardData
 		"Monitor": return MonitorCardData
-	
-	
-	
 
 
 func sort_data_on_type(type, dict):
 	for key in dict.keys():
-			if dict[key]["Basis / Prof"] == type:
-				dict.erase(key)
-	print(dict.keys())
+		if dict[key]["Basis / Prof"] == type:
+			dict.erase(key)
 
 
 func card_pop(dict, key):
 	dict.erase(key)
-	print("card popped")
-	print(dict.keys())
-	
-
-func import_data():
-	load_json_data()
-	
-	print("card data created")
-	for Card in CardData:
-		if Card["Type"] == "Question" or Card["Type"] == "Discussion":
-			if Card["Subtype"] == "Plan":
-				PlanCardData[int(Card["UID"])] = Card
-	
-	for Card in CardData:
-		if Card["Type"] == "Question" or Card["Type"] == "Discussion":
-			if Card["Subtype"] == "Build":
-				BuildCardData[int(Card["UID"])] = Card
-				
-	for Card in CardData:
-		if Card["Type"] == "Question" or Card["Type"] == "Discussion":
-			if Card["Subtype"] == "Test":
-				TestCardData[int(Card["UID"])] = Card
-				
-	for Card in CardData:
-		if Card["Type"] == "Question" or Card["Type"] == "Discussion":
-			if Card["Subtype"] == "Deploy":
-				DeployCardData[int(Card["UID"])] = Card
-				
-	for Card in CardData:
-		if Card["Type"] == "Question" or Card["Type"] == "Discussion":
-			if Card["Subtype"] == "Code":
-				CodeCardData[int(Card["UID"])] = Card
-				
-	for Card in CardData:
-		if Card["Type"] == "Question" or Card["Type"] == "Discussion":
-			if Card["Subtype"] == "Release":
-				ReleaseCardData[int(Card["UID"])] = Card
-				
-	for Card in CardData:
-		if Card["Type"] == "Question" or Card["Type"] == "Discussion":
-			if Card["Subtype"] == "Operate":
-				OperateCardData[int(Card["UID"])] = Card
-				
-	for Card in CardData:
-		if Card["Type"] == "Question" or Card["Type"] == "Discussion":
-			if Card["Subtype"] == "Monitor":
-				MonitorCardData[int(Card["UID"])] = Card
-			
-	#for each in CardData:
-		#if(CardData[each][1] == "Build"):
-			#print(CardData[each])
-			
-	#print("#### build card data: ####")
-	#for each in BuildCardData:
-		#print(BuildCardData[each])
-		#if(CardData[each][1] == "Build"):
-			#print(CardData[each])
-		#for every in CardData[each]:
-			#print(every[0])
-			
-	
-	print("--")
-	#var file = FileAccess.open(path,FileAccess.READ)
-	#file.open(path, file.READ)
-	#while !file.eof_reached():
-		#var data_set = Array(file.get_csv_line())
-	#file.close()
--- scripts/UX/SlideOutManager.gd
+++ scripts/UX/SlideOutManager.gd
@@ -1,34 +1,111 @@
 extends Control
 
+signal data_ready
+
 var PATH_JSON_DATA = "res://Data/slideouts.json"
-var SlideOutData : Array
+var WEB_PATH = "Data/slideouts.json"
+var SlideOutData : Array = []
+var data_loaded := false
 
 @export var GuideName : String
 @export var GuideGrid : GridContainer
 
 var guideFieldPrefab = preload("res://prefabs/guide_field.tscn")
 
+
+func ensure_ready():
+	if data_loaded:
+		return
+	await data_ready
+
+
 func _ready() -> void:
-	setup_fields()
+	load_json_data()
+
 
 func load_json_data():
-	#Find the json file
-	var file = PATH_JSON_DATA
-	#Read the json file and parse it as a string
-	var json_as_text = FileAccess.get_file_as_string(file)
+	if OS.has_feature("web"):
+		load_json_web()
+	else:
+		load_json_local()
+		finish_loading()
+
+
+func finish_loading():
+	setup_fields()
+	data_loaded = true
+	emit_signal("data_ready")
+
+
+func load_json_local():
+	var json_as_text = FileAccess.get_file_as_string(PATH_JSON_DATA)
+
+	if json_as_text == "" or json_as_text == null:
+		SlideOutData = []
+		return
+
 	var json = JSON.parse_string(json_as_text)
-	#Set the json string as an array
+
+	if json == null:
+		SlideOutData = []
+		return
+
 	SlideOutData = Array(json)
-	
+
+
+func get_web_base() -> String:
+	var protocol := "http"
+	var host := "localhost"
+
+	if OS.has_feature("web"):
+		var js := JavaScriptBridge
+		protocol = "https" if js.eval("window.location.protocol") == "https:" else "http"
+		host = js.eval("window.location.hostname")
+
+	return "%s://%s/" % [protocol, host]
+
+
+func load_json_web():
+	var base = get_web_base()
+	var url = base + WEB_PATH + "?t=" + str(Time.get_unix_time_from_system())
+
+	var http = HTTPRequest.new()
+	add_child(http)
+
+	http.request_completed.connect(_on_web_json_loaded)
+
+	if http.request(url) != OK:
+		load_json_local()
+		finish_loading()
+
+
+func _on_web_json_loaded(result, response_code, headers, body):
+	var text = body.get_string_from_utf8()
+
+	if result != HTTPRequest.RESULT_SUCCESS or response_code != 200:
+		load_json_local()
+		finish_loading()
+		return
+
+	var json = JSON.parse_string(text)
+
+	if json == null:
+		load_json_local()
+		finish_loading()
+		return
+
+	SlideOutData = Array(json)
+	finish_loading()
+
 
 func setup_fields():
-	load_json_data()
 	for guide in GuideGrid.get_children():
 		guide.queue_free()
+
 	for SlideOut in SlideOutData:
 		if SlideOut["GuideName"] == GuideName:
 			var NrOfFields : int = int(SlideOut["NrFields"])
-			for i  in range(1,NrOfFields+1):
+			for i in range(1, NrOfFields + 1):
 				var guidefield = guideFieldPrefab.instantiate()
 				GuideGrid.add_child(guidefield)
 				var GuideFieldManager : GuideField = guidefield as GuideField
@@ -37,4 +114,4 @@ func setup_fields():
 				var GuideColor = SlideOut["ColorIconField #%d" % i]
 				var GuideTitle = SlideOut["Title Field #%d" % i]
 				var GuideSubtext = SlideOut["Text Field #%d" % i]
-				GuideFieldManager._setup_data(GuideIcon,GuideColor,GuideTitle,GuideSubtext)
+				GuideFieldManager._setup_data(GuideIcon, GuideColor, GuideTitle, GuideSubtext)
